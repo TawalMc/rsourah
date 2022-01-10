5d9397d25d72c0d78e679b532d2ede11f5d7046f
TawalMc <developtawaliou@gmail.com>
2022-01-10 12:06:46 +0100

    nothing

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home(props) {
         <Text color="white" fontSize="xl" opacity={0.8}>
           <Trans id="tr-app_goal">
             Parfois il est difficile de choisir la sourate à lire lors de la
-            prière, laissez alors notre application vous choisir ces 2 sourates.
+            prière, laissez alors notre application vous choisir ces 2 sourates
           </Trans>
         </Text>
         <HStack marginTop="30px">
